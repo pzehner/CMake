918bf7382a2a31507955bf5d4f2e12b1b22fdd6b
Paul Zehner <zehner.paul@jaxa.jp>
2019-06-27 11:32:35 +0900

    Adding basic support for Fujitsu CXX compiler

--- Modules/Compiler/Fujitsu-DetermineCompiler.cmake
+++ Modules/Compiler/Fujitsu-DetermineCompiler.cmake
@@ -1,2 +1,9 @@
 
 set(_compiler_id_pp_test "defined(__FUJITSU) || defined(__FCC_VERSION) || defined(__fcc_version)")
+
+set(_compiler_id_version_compute "
+# if defined(__FCC_VERSION)
+#   define @PREFIX@COMPILER_VERSION_MAJOR @MACRO_DEC@(__FCC_VERSION / 100)
+#   define @PREFIX@COMPILER_VERSION_MINOR @MACRO_DEC@(__FCC_VERSION % 100 / 10)
+#   define @PREFIX@COMPILER_VERSION_PATCH @MACRO_DEC@(__FCC_VERSION % 10)
+# endif")
